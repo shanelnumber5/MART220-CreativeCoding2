a642132723c14b8b1627f9c22c1b9e4e31136fc4
Shanel Locke <77364986+shanelnumber5@users.noreply.github.com>
2022-03-29 10:39:21 -0600

    Update

---
+++ homework-11/scripts/myLotion.js
@@ -0,0 +1,30 @@
+class myLotion{
+
+    constructor(radius, height, detailX, detailY, locationX, locationY)
+    {
+        this.radius = radius;
+        this.height = height;
+        this.detailX = detailX;
+        this.detailY = detailY;
+        this.locationX = locationX;
+        this.locationY = locationY;
+    }
+
+    show()
+    {
+        cylinder(this.radius, this.height, this.detailX, this.detailY);
+    }
+
+    translateCylinder()
+    {
+        translate(this.locationX,this.locationY);
+    }
+
+    rotateCylinder(amount)
+    {
+        rotateX(amount);
+        rotateY(amount);
+    }
+
+
+}
---
+++ homework-11/scripts/sketch.js
@@ -0,0 +1,52 @@
+var x = 0;
+var y = 0;
+var z = 250;
+let purse;
+var lotions = [];
+var speed = -1;
+var locationX = -200;
+var locationY = -200;
+
+
+function preload() {
+  buffalo = loadImage('assets/buffalo.jpg');
+  purse = loadModel('assets/10130_BagPurse_v3_L3.obj', true);
+  label = loadImage('assets/label.png');
+  skin = loadImage('assets/skin.jpg');
+}
+
+function setup() {
+  createCanvas(displayWidth, displayHeight, WEBGL);
+
+  for (let i = 0; i < 2; i++) {
+    let myL = new myLotion(40, 100, 24, 24, locationX, locationY);
+    lotions.push(myL);
+    locationX += 200;
+    locationY += 0;
+  }
+}
+
+function draw() {
+  background(255);
+  image(buffalo, -1100, -550, 1075, 850);
+  texture(skin);
+
+  translate(300, -200);
+  //skin purse
+  scale(3);
+  rotateX(frameCount * 0.001);
+  rotateY(frameCount * 0.001);
+  model(purse);
+  texture(label);
+  noStroke()
+
+
+  for (let i = 0; i < lotions.length; i++) {
+    push();
+    lotions[i].translateCylinder();
+    lotions[i].rotateCylinder(frameCount * .006);
+    lotions[i].show();
+    pop();
+  }
+
+}
